ff6f12ca1f7f28f479eb1878111440720753a3bb
Pistoy <emrecekic73@gmail.com>
2024-04-25 23:03:57 +0300

    Initial Commit

---
+++ KodecoPlayground/KodecoPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodecoPlaygroundApp.swift
+//  KodecoPlayground
+//
+//  Created by Emre Cekic on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct KodecoPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
